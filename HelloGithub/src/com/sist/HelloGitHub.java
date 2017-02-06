11235c09debf0cd2a0ec973df04bf079decc8291
hyunna <hyunna7044@naver.com>
2017-02-06 15:17:46 +0900

    by hunna

--- HelloGithub/src/com/sist/HelloGitHub.java
+++ HelloGithub/src/com/sist/HelloGitHub.java
@@ -10,6 +10,5 @@ public class HelloGitHub {
 		System.out.println("Confilt Test");
 		System.out.println("fetched");
 		System.out.println("Pushed");
-		System.out.println("Git pull testing");
 	}
 }
